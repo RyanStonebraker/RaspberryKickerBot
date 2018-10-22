2b52122713e382a26420b225277f3b6d16e0254a
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-10-22 15:01:00 -0800

    Adds connection status toggler

--- app/js/robotController.js
+++ app/js/robotController.js
@@ -1,20 +1,38 @@
 function RobotController (robot) {
   this.robot = robot;
-  this.initConnection();
+  let self = this;
+  let persistentConnCheck = setInterval(function () {
+    self.initConnection();
+    if (self.status.connected)
+      clearInterval(persistentConnCheck);
+  }, 100);
 }
 
 RobotController.prototype.status = {
-  connected: false
+  connected: false,
+  errorMessage: "Disconnected"
 };
 
 RobotController.prototype.initConnection = function () {
   // TODO: Establish handshake with robot
   this.status.connected = true;
+  
+  let statusArea = document.querySelector("section.status h2");
+  if (this.status.connected) {
+    statusArea.textContent = "Connected";
+    statusArea.classList.remove("error");
+    statusArea.classList.add("message");
+  }
+  else {
+    statusArea.textContent = this.status.errorMessage;
+    statusArea.classList.remove("message");
+    statusArea.classList.add("error");
+  }
 }
 
 RobotController.prototype.forward = function () {
-  let xMove = 5 * Math.sin(this.robot.angle * Math.PI / 180);
-  let yMove = -5 * Math.cos(this.robot.angle * Math.PI / 180);
+  let xMove = 20 * Math.sin(this.robot.angle * Math.PI / 180);
+  let yMove = -20 * Math.cos(this.robot.angle * Math.PI / 180);
   if (!this.status.connected) {
     this.robot.screenPosition.x += xMove;
     this.robot.screenPosition.y += yMove;
@@ -27,8 +45,8 @@ RobotController.prototype.forward = function () {
 }
 
 RobotController.prototype.backward = function () {
-  let xMove = -5 * Math.sin(this.robot.angle * Math.PI / 180);
-  let yMove = 5 * Math.cos(this.robot.angle * Math.PI / 180);
+  let xMove = -20 * Math.sin(this.robot.angle * Math.PI / 180);
+  let yMove = 20 * Math.cos(this.robot.angle * Math.PI / 180);
   if (!this.status.connected) {
     this.robot.screenPosition.x += xMove;
     this.robot.screenPosition.y += yMove;
